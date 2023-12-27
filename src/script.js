398c18ed108afe7fc69bc8a4c66aae7d72d183a3
Yauvan <54529920+VirtualMonk-108@users.noreply.github.com>
2023-12-27 01:42:08 +0100

    looked at scaling, rotation and camera lookAt

--- src/script.js
+++ src/script.js
@@ -10,17 +10,37 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 'red'})
 const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Position
 // mesh.position.x = 0.7
 // mesh.position.y = -0.5
 // mesh.position.z = 1
 //instead of doing the above, we can do the following
 mesh.position.set(0.7, -0.6, 1)
 
-scene.add(mesh)
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+//instead of doing the above, we can do the following
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+// PI is 180 degrees so half a rotation || Full rotation will be 2 * PI
+// mesh.rotation.reorder('YXZ') // this will change the order of the rotation to avoid gimbal lock
+// also we have to do reorder before we do the rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
+
+
+
 
 //Axes helper
-// const axesHelper = new THREE.AxesHelper()
-// scene.add(axesHelper)
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 // this is the distance from the origin to the mesh. origin is 0,0,0 which is the center of the scene
 // console.log(mesh.position.length()) 
@@ -36,6 +56,10 @@ camera.position.z = 3
 // camera.position.x = 1
 // camera.position.y = 1
 scene.add(camera)
+
+// camera.lookAt(mesh.position) - this will 
+// make the camera look at the mesh from whatever position it is in
+camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 // this is the distance from the camera to the mesh
 // console.log(mesh.position.length(camera.position)) 
